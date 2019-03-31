cf1c4d86fa4f17e424644aafe0bbe99f7df8026a
StefaniaAndries <48429985+StefaniaAndries@users.noreply.github.com>
2019-03-31 12:19:11 +0300

    Add files via upload

---
+++ viewEmployeesClasses/Subordinate.java
@@ -0,0 +1,19 @@
+import java.util.Vector;
+
+public class Subordinate extends Employee {
+
+  public String IDSuperior;
+
+    /**
+   * 
+   * @element-type Superior
+   */
+  public Vector  view;
+
+  public void getInformation() {
+  }
+
+  public void getIDSuperior() {
+  }
+
+}
