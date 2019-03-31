b00d376011969caabd4140a4b439252298b6e44f
anaturcu <48413771+anaturcu@users.noreply.github.com>
2019-03-31 11:22:29 +0300

    Add files via upload

---
+++ FireClasses/Person.java
@@ -0,0 +1,48 @@
+abstract class Person {
+
+  private String CNP;
+
+  public String firstName;
+
+  public String lastName;
+
+  public String gendre;
+
+  public String adress;
+
+  public String getCNP() {
+  return null;
+  }
+
+  public String getFirstName() {
+  return null;
+  }
+
+  public String getLastName() {
+  return null;
+  }
+
+  public String getGendre() {
+  return null;
+  }
+
+  public String getAdress() {
+  return null;
+  }
+
+  public void setFirsName(String firstName) {
+  }
+
+  public void setLastName(String lastName) {
+  }
+
+  public void setAdress(String adress) {
+  }
+
+  public void setCNP(String CNP) {
+  }
+
+  public void setGendre(String gendre) {
+  }
+
+}
