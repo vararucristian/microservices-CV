61c3deff210a6580505d6fe45e7bea1c71118283
BalanBogdan98 <48065420+BalanBogdan98@users.noreply.github.com>
2019-03-31 12:23:55 +0300

    Add files via upload

---
+++ ViewProfileClasses/Admin.java
@@ -0,0 +1,9 @@
+public class Admin extends Account {
+
+  private void View_account( account_id) {
+  }
+
+  public void Delete_account( account_id) {
+  }
+
+}
---
+++ ViewProfileClasses/Profile.java
@@ -0,0 +1,56 @@
+import java.util.Vector;
+
+public class Profile {
+
+  public String first_name;
+
+  public String last_name;
+
+  public String Position_in_company;
+
+  public image picture;
+
+  public String email;
+
+  public String Home_adress;
+
+  public Integer account_id;
+
+  public String user_name;
+
+    public Vector  myAccount;
+    public Account views;
+    public Vector  views;
+
+  public void report_profile( account_id) {
+  }
+
+  public string get_first_name( first_name) {
+  return null;
+  }
+
+  public Integer get_account_id( account_id) {
+  return null;
+  }
+
+  public String get_user_name( user_name) {
+  return null;
+  }
+
+  public String get_last_name( last_name) {
+  return null;
+  }
+
+  public String get_email( email) {
+  return null;
+  }
+
+  public String get_Position_in_company( Position_in_company) {
+  return null;
+  }
+
+  public String get_Home_adress( Home_adress) {
+  return null;
+  }
+
+}
