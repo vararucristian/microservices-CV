4990f5a097eb51d6f540d59278e17d80d41610a1
anaturcu <48413771+anaturcu@users.noreply.github.com>
2019-03-31 11:18:29 +0300

    Add files via upload

---
+++ HireClasses/Superior.java
@@ -0,0 +1,32 @@
+import java.util.Vector;
+
+public class Superior extends Employee {
+
+  public Integer nrOfSubordinates;
+
+    /**
+   * 
+   * @element-type Applicant
+   */
+  public Vector  possibleEmployee;
+    /**
+   * 
+   * @element-type Employee
+   */
+  public Vector  myEmployee;
+
+  public Void viewCV(Integer CVNumber) {
+  return null;
+  }
+
+  public void analyzeAplicant(String applicationCode) {
+  }
+
+  public void hire(Applicant applicant) {
+  }
+
+  public Integer getNrOfSubordinates() {
+  return null;
+  }
+
+}
