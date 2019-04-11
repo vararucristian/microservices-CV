e0f007ffb673669aa76aa6bcc311b7efabf50347
BalanBogdan98 <48065420+BalanBogdan98@users.noreply.github.com>
2019-04-11 14:06:59 +0300

    Update Inserare.sql

--- Profile database/Inserare.sql
+++ Profile database/Inserare.sql
@@ -1,4 +1,4 @@
-insert into profile values ('Dani', 'Lica', 'danutu698yahoo.com', 'Strada Olyului nr 45','dani56', 'Software', 101, 30, 'male' );
-insert into profile values ('Marin', 'Balan', 'marin76@yaho.com', 'Strada Olari nr 56', 'marin78', 'Hardware', 102, 45, 'male' );
-insert into profile values ('Petrica', 'Anton', 'anton.petrica@yahoo,com', 'Strada Siret nr 67', 'petrica.anton', 'working student', 103, 24, 'male' );
-insert into profile values ('Sorin', 'Balan', 'balan.sorin@yahoo.com', 'Strada Rodie nr 56', 'balan.sorin', 'team lider', 104, 24, 'male' );
+insert into profile values ('Dani', 'Lica', 'danutu698yahoo.com', 'Strada Olyului nr 45','dani56', 'Software', 101, 30, 'male',1 );
+insert into profile values ('Marin', 'Balan', 'marin76@yaho.com', 'Strada Olari nr 56', 'marin78', 'Hardware', 102, 45, 'male',2 );
+insert into profile values ('Petrica', 'Anton', 'anton.petrica@yahoo,com', 'Strada Siret nr 67', 'petrica.anton', 'working student', 103, 24, 'male' ,3);
+insert into profile values ('Sorin', 'Balan', 'balan.sorin@yahoo.com', 'Strada Rodie nr 56', 'balan.sorin', 'team lider', 104, 24, 'male' ,1);
