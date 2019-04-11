3a76767051e8a169753aa86f61f517e4587a2f87
BalanBogdan98 <48065420+BalanBogdan98@users.noreply.github.com>
2019-04-11 13:02:55 +0300

    Add files via upload

---
+++ Creareprofile.sql
@@ -0,0 +1,19 @@
+create database if not exists profile;
+
+use profile;
+
+drop table if exists profile;
+
+create table profile (
+	name varchar(100) NOT NULL ,
+	last_name varchar(45) NOT NULL,
+	email varchar(45) NOT NULL,
+	adress varchar(45) NOT NULL,
+	username varchar(45) NOT NULL,
+	position_in_company varchar(45) NOT NULL,
+	account_id int primary key,
+	age int NOT NULL,
+	gender varchar(45) NOT NULL
+	);
+
+describe profile;
