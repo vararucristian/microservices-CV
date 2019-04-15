c2ae04ba6402698c8dbb04bccb069c78d14a8374
CristianVararu <vararucristian@yahoo.com>
2019-04-15 08:01:14 +0300

    Employees

--- ChangeProfileClasses/Admin.java
+++
@@ -1,9 +0,0 @@
-public class Admin extends Account {
-
-  public void viewAcc( id_account) {
-  }
-
-  public void delAcc( id_account) {
-  }
-
-}
--- ViewProfileClasses/Admin.java
+++
@@ -1,9 +0,0 @@
-public class Admin extends Account {
-
-  private void View_account( account_id) {
-  }
-
-  public void Delete_account( account_id) {
-  }
-
-}
