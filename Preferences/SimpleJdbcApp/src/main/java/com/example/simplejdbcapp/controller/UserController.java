612856ae2c21427ad3cb0a120620d1cced2c0295
stevcelstev <phantomguy004@yahoo.com>
2019-04-20 16:54:32 +0300

    Add files via upload

--- Preferences/SimpleJdbcApp/src/main/java/com/example/simplejdbcapp/controller/UserController.java
+++ Preferences/SimpleJdbcApp/src/main/java/com/example/simplejdbcapp/controller/UserController.java
@@ -38,32 +38,42 @@ public class UserController {
  }
  
  @GetMapping(path = "/changePreference/08_10/{pref}/{accId}/{taskId}")
- public String changeH810(@PathVariable("pref") String pref,@PathVariable("accId") int accId, @PathVariable("taskId") int taskId) {
-     return (String) userRepository.changeH810(pref, accId, taskId);
+ public int changeH810(@PathVariable("pref") String pref,@PathVariable("accId") int accId, @PathVariable("taskId") int taskId) {
+     return userRepository.changeH810(pref, accId, taskId);
  }
  
  @GetMapping(path = "/changePreference/10_12/{pref}/{accId}/{taskId}")
- public String changeH1012(@PathVariable("pref") String pref,@PathVariable("accId") int accId, @PathVariable("taskId") int taskId) {
-     return (String) userRepository.changeH1012(pref, accId, taskId);
+ public int changeH1012(@PathVariable("pref") String pref,@PathVariable("accId") int accId, @PathVariable("taskId") int taskId) {
+     return userRepository.changeH1012(pref, accId, taskId);
  }
  
  @GetMapping(path = "/changePreference/12_14/{pref}/{accId}/{taskId}")
- public String changeH1214(@PathVariable("pref") String pref,@PathVariable("accId") int accId, @PathVariable("taskId") int taskId) {
-     return (String) userRepository.changeH1214(pref, accId, taskId);
+ public int changeH1214(@PathVariable("pref") String pref,@PathVariable("accId") int accId, @PathVariable("taskId") int taskId) {
+     return userRepository.changeH1214(pref, accId, taskId);
  }
  
  @GetMapping(path = "/changePreference/14_16/{pref}/{accId}/{taskId}")
- public String changeH1416(@PathVariable("pref") String pref,@PathVariable("accId") int accId, @PathVariable("taskId") int taskId) {
-     return (String) userRepository.changeH1416(pref, accId, taskId);
+ public int changeH1416(@PathVariable("pref") String pref,@PathVariable("accId") int accId, @PathVariable("taskId") int taskId) {
+     return userRepository.changeH1416(pref, accId, taskId);
  }
  
  @GetMapping(path = "/changePreference/16_18/{pref}/{accId}/{taskId}")
- public String changeH1618(@PathVariable("pref") String pref,@PathVariable("accId") int accId, @PathVariable("taskId") int taskId) {
-     return (String) userRepository.changeH1618(pref, accId, taskId);
+ public int changeH1618(@PathVariable("pref") String pref,@PathVariable("accId") int accId, @PathVariable("taskId") int taskId) {
+     return userRepository.changeH1618(pref, accId, taskId);
  }
  
  @GetMapping(path = "/changePreference/18_20/{pref}/{accId}/{taskId}")
- public String changeH1820(@PathVariable("pref") String pref,@PathVariable("accId") int accId, @PathVariable("taskId") int taskId) {
-     return (String) userRepository.changeH1820(pref, accId, taskId);
+ public int changeH1820(@PathVariable("pref") String pref,@PathVariable("accId") int accId, @PathVariable("taskId") int taskId) {
+     return userRepository.changeH1820(pref, accId, taskId);
  }
+ 
+ @GetMapping(path = "/deletePreference/{accId}")
+ public int deletePref(@PathVariable("accId") int accId) {
+	 return userRepository.deletePref(accId);
+ }
+ 
+ @GetMapping(path = "/createPreference/{acc_id}/{h08_10}/{h10_12}/{h12_14}/{h14_16}/{h16_18}/{h18_20}")
+ public int createPref(@PathVariable("acc_id") int acc_id, @PathVariable("h08_10") String h08_10, @PathVariable("h10_12") String h10_12, @PathVariable("h12_14") String h12_14, @PathVariable("h14_16") String h14_16, @PathVariable("h16_18") String h16_18, @PathVariable("h18_20") String h18_20) {
+	return userRepository.createPref(acc_id, h08_10, h10_12, h12_14, h14_16, h16_18, h18_20); 
+}
 }
