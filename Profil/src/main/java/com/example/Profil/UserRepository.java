ace369fe2e2c513ba9b65daeaf1b1b6e7872e185
BalanBogdan98 <48065420+BalanBogdan98@users.noreply.github.com>
2019-04-22 16:37:04 +0300

    Update UserRepository.java

--- Profil/src/main/java/com/example/Profil/UserRepository.java
+++ Profil/src/main/java/com/example/Profil/UserRepository.java
@@ -15,24 +15,21 @@ import javax.sql.DataSource;
 @Repository
 public class UserRepository {
 
-    // we are autowiring jdbc template,
-    // using the properties we have configured spring automatically
-    // detects and creates jdbc template using the configuration
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
 
-    public JSONObject getAllUserNames(String getusernames) {
+    public JSONObject getInfoProfile(String userID) {
         List<String> userNameList = new ArrayList<>();
-        userNameList.addAll(jdbcTemplate.queryForList("select name from profile where account_id="+getusernames, String.class));
-        userNameList.addAll(jdbcTemplate.queryForList("select last_name from profile where account_id="+getusernames, String.class));
-        userNameList.addAll(jdbcTemplate.queryForList("select email from profile where account_id="+getusernames, String.class));
-        userNameList.addAll(jdbcTemplate.queryForList("select adress from profile where account_id="+getusernames, String.class));
-        userNameList.addAll(jdbcTemplate.queryForList("select username from profile where account_id="+getusernames, String.class));
-        userNameList.addAll(jdbcTemplate.queryForList("select position_in_company from profile where account_id="+getusernames, String.class));
-        userNameList.addAll(jdbcTemplate.queryForList("select account_id from profile where account_id="+getusernames, String.class));
-        userNameList.addAll(jdbcTemplate.queryForList("select age from profile where account_id="+getusernames, String.class));
-        userNameList.addAll(jdbcTemplate.queryForList("select gender from profile where account_id="+getusernames, String.class));
+        userNameList.addAll(jdbcTemplate.queryForList("select name from profile where account_id="+userID, String.class));
+        userNameList.addAll(jdbcTemplate.queryForList("select last_name from profile where account_id="+userID, String.class));
+        userNameList.addAll(jdbcTemplate.queryForList("select email from profile where account_id="+userID, String.class));
+        userNameList.addAll(jdbcTemplate.queryForList("select adress from profile where account_id="+userID, String.class));
+        userNameList.addAll(jdbcTemplate.queryForList("select username from profile where account_id="+userID, String.class));
+        userNameList.addAll(jdbcTemplate.queryForList("select position_in_company from profile where account_id="+userID, String.class));
+        userNameList.addAll(jdbcTemplate.queryForList("select account_id from profile where account_id="+userID, String.class));
+        userNameList.addAll(jdbcTemplate.queryForList("select age from profile where account_id="+userID, String.class));
+        userNameList.addAll(jdbcTemplate.queryForList("select gender from profile where account_id="+userID, String.class));
 
 
         JSONObject profile= new JSONObject();
@@ -53,59 +50,59 @@ public class UserRepository {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public String changename(String dorinta,int getusernames) {
+    public String changename(String dorinta,int userID) {
         this.jdbcTemplate.update(
                 "update profile set name=? where account_id = ?",
-                dorinta,getusernames);
+                dorinta,userID);
         return "Succes";
     }
 
-    public String changeprenume(String dorinta,int getusernames) {
+    public String changeprenume(String dorinta,int userID) {
         this.jdbcTemplate.update(
                 "update profile set last_name=? where account_id = ?",
-                dorinta,getusernames);
+                dorinta,userID);
         return "Succes";
     }
 
-    public String changeemail(String dorinta,int getusernames) {
+    public String changeemail(String dorinta,int userID) {
         this.jdbcTemplate.update(
                 "update profile set email=? where account_id = ?",
-                dorinta,getusernames);
+                dorinta,userID);
         return "Succes";
     }
 
-    public String changeadress(String dorinta,int getusernames) {
+    public String changeadress(String dorinta,int userID) {
         this.jdbcTemplate.update(
                 "update profile set adress=? where account_id = ?",
-                dorinta,getusernames);
+                dorinta,userID);
         return "Succes";
     }
 
-    public String changeusername(String dorinta,int getusernames) {
+    public String changeusername(String dorinta,int userID) {
         this.jdbcTemplate.update(
                 "update profile set username=? where account_id = ?",
-                dorinta,getusernames);
+                dorinta,userID);
         return "Succes";
     }
 
-    public String changeposition(String dorinta,int getusernames) {
+    public String changeposition(String dorinta,int userID) {
         this.jdbcTemplate.update(
                 "update profile set position_in_company=? where account_id = ?",
-                dorinta,getusernames);
+                dorinta,userID);
         return "Succes";
     }
 
-    public String changeage(String dorinta,int getusernames) {
+    public String changeage(String dorinta,int userID) {
         this.jdbcTemplate.update(
                 "update profile set age=? where account_id = ?",
-                dorinta,getusernames);
+                dorinta,userID);
         return "Succes";
     }
 
-    public String changegender(String dorinta,int getusernames) {
+    public String changegender(String dorinta,int userID) {
         this.jdbcTemplate.update(
                 "update profile set gender=? where account_id = ?",
-                dorinta,getusernames);
+                dorinta,userID);
         return "Succes";
     }
 
