8b5f226fe8c843698cb90eca485a21fc358c8f80
CristianVararu <vararucristian@yahoo.com>
2019-04-22 15:14:41 +0300

    xm;

--- employees/src/main/java/com/employees/employees/resources/EmployeesResource.java
+++ employees/src/main/java/com/employees/employees/resources/EmployeesResource.java
@@ -13,6 +13,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import static com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat.URI;
+
 
 @RestController
 @RequestMapping("/employees")
