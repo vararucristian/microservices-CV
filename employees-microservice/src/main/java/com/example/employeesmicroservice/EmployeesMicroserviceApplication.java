9884f4a2b4bfbae61bc4b7ff5bfddcad0b70b4ec
anaturcu <48413771+anaturcu@users.noreply.github.com>
2019-05-03 12:37:17 +0300

    micros

---
+++ employees-microservice/src/main/java/com/example/employeesmicroservice/EmployeesMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeesmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesMicroserviceApplication.class, args);
+	}
+
+}
---
+++ employees-microservice/src/main/java/com/example/employeesmicroservice/EmployeesMicroserviceCollector.java
@@ -0,0 +1,48 @@
+package com.example.employeesmicroservice;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeesMicroserviceCollector {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    private Boolean verifyId(int id)
+    {
+        Boolean exists= true;
+        String verifyIdStatement = "SELECT count(id) from employee where id = " + id;
+        int result =  jdbcTemplate.queryForObject(verifyIdStatement, Integer.class);
+        if (result==0)
+            exists=false;
+        return exists;
+    }
+    @RequestMapping("/tryGet/{idEmployee}")
+    public ResponseEntity<String> verifyPosition(@PathVariable("idEmployee") int idEmployee) throws URISyntaxException {
+        URI location = new URI("localhost");
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setLocation(location);
+        responseHeaders.set("MyResponseHeader", "MyValue");
+        if (verifyId(idEmployee) == false)
+            return new ResponseEntity<String>("The id " + idEmployee + " does not exist.", responseHeaders, HttpStatus.NOT_FOUND);
+        else {
+            String selectPositionStatement = "SELECT position from employee where id = " + idEmployee;
+            String position = jdbcTemplate.queryForObject(selectPositionStatement, String.class);
+            if (!position.equals("human resurces"))
+                return new ResponseEntity<String>("The employee with  id "+ idEmployee+ " is not allowed to make changes.", responseHeaders, HttpStatus.METHOD_NOT_ALLOWED);
+            else
+
+        }
+    }
+}
