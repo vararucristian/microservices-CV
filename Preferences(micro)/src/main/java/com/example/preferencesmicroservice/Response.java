90a57ef62ebcd9825c15b7d43a8a23143e1d0cc1
vararucristian <48356098+vararucristian@users.noreply.github.com>
2019-05-10 19:46:50 +0300

    oracel

---
+++ Preferences(micro)/src/main/java/com/example/preferencesmicroservice/Response.java
@@ -0,0 +1,19 @@
+package com.example.preferencesmicroservice;
+
+public class Response {
+    private int exitCode;
+    private String message;
+
+    public Response(int exitCode, String message) {
+        this.exitCode = exitCode;
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "{\n" +
+                "\t\"exitCode\": " + exitCode +
+                ",\n\t\"message\": \"" + message + "\"" +
+                "\n}";
+    }
+}
